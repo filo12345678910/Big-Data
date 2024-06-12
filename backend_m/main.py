6be1ad05a60ffc6ecc0e5b5b9e9a215338578de5
filo12345678910 <mammammam1212@gmail.com>
2024-06-13 00:16:49 +0200

    working all simple requirements

--- backend_m/main.py
+++ backend_m/main.py
@@ -2,4 +2,44 @@ from connection import session
 import service
 service.start(session)
 while 1:
-    input()
+    print("""
+0 - show entire database
+1 - make a reservation
+2 - update a reservation
+3 - see reservation by name
+4 - see reservation by seat and train id
+          """)
+    x = input()
+    if x == '0':
+        service.query_data(session, 'trains')
+        service.query_data(session, 'trainss')
+    if x == '1':
+        print("Provide a train id and a name")
+        y = input()
+        if len(y.split()) != 2:
+            print("Invalid Input")
+        else:
+            service.input_1(session, int(y.split()[0]), y.split()[1])
+    if x == '2':
+        print("Provide an old name, and a new name to update")
+        y = input()
+        if len(y.split()) != 2:
+            print("Invalid Input")
+        else:
+            service.input_2(session, y.split()[0], y.split()[1])
+    if x == '3':
+        print("Provide a name")
+        y = input()
+        if len(y.split()) != 1:
+            print("Invalid Input")
+        else:
+            service.input_3(session, y.split()[0])
+    if x == '4':
+        print("Provide a train and seat number")
+        y = input()
+        if len(y.split()) != 2:
+            print("Invalid Input")
+        else:
+            service.input_4(session, y.split()[0], y.split()[1])
+    else:
+        print("Invalid Input")
--- backend_m/service.py
+++ backend_m/service.py
@@ -25,11 +25,45 @@ def start(session):
     clear_table(session, 'trains')
     clear_table(session, 'trainss')
 
-    for i in range(10):
+    for i in range(2):
         capacity = random.randint(20, 40)
         insert_data_trains(session, i, capacity)
         for j in range(capacity):
             insert_data_trainss(session, j, i, 'none')
 
-    query_data(session, 'trains')
-    query_data(session, 'trainss')
+def input_1(session, train_id, name):
+    result = session.execute("SELECT * FROM trains WHERE id = %s", (train_id, ))
+    if not result.one():
+        print("There is no train with this id")
+        return 0
+    if result[0][1] == 0:
+        print("There are no more seats left on this train")
+        return 0
+    session.execute("""UPDATE trains SET capacity = %s WHERE id = %s""", (result[0][1] - 1, train_id))
+    result_2 = session.execute("SELECT * FROM trainss WHERE train_id = %s AND name = 'none' ALLOW FILTERING", (train_id,))
+    session.execute("""UPDATE trainss SET name = %s
+    WHERE seat_id = %s AND train_id = %s""", (name, result_2[0][0], train_id))
+    print(f'You have succesfully reserved seat number: {result_2[0][0]}')
+    return 1
+
+def input_2(session, old_name, new_name):
+    result = session.execute("SELECT * FROM trainss WHERE name = %s ALLOW FILTERING", (old_name, ))
+    if not result.one():
+        print("There is no reservation with this name")
+        return 0
+    row = result.one()
+    seat_id, train_id = row.seat_id, row.train_id
+    session.execute("""UPDATE trainss SET name = %s 
+    WHERE seat_id = %s AND train_id = %s""", (new_name, seat_id, train_id))
+    print(f'You have succesfully changed a name from: {old_name} to {new_name}')
+    return 1
+
+def input_3(session, name):
+    result = session.execute("SELECT * FROM trainss WHERE name = %s ALLOW FILTERING", (name, ))
+    if not result.one():
+        print("There is no reservation with this name")
+        return 0
+    row = result.one()
+    seat_id, train_id = row.seat_id, row.train_id
+    print(f"The reservation on the name: {name}, has train number: {train_id} and seat number: {seat_id}")
+    return 1
