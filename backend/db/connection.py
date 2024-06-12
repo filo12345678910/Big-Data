73321aa311e011fdbe021fff63bf797f97fa186d
filo12345678910 <mammammam1212@gmail.com>
2024-06-13 00:39:48 +0200

    refactorization

--- backend/db/connection.py
+++
@@ -1,4 +0,0 @@
-from cassandra.cluster import Cluster
-
-cluster = Cluster(["127.0.0.1"])
-session = cluster.connect("cinema")
--- backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers import movies, shows, reservations
-
-app = FastAPI()
-
-app.include_router(shows.router)
-app.include_router(movies.router)
-app.include_router(reservations.router)
